f9c7637d1ab9eb699f5e18f490c039a32d0171de
Adetoye <samuel.adetoye@thepalladiumgroup.com>
2024-04-05 12:00:09 -0700

    newFix

--- src/main/java/net/myplugin/PluginAApplication.java
+++ src/main/java/net/myplugin/PluginAApplication.java
@@ -4,13 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import net.mycore.demo.PluginInterface;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @SpringBootApplication
+@Component
 public class PluginAApplication  implements PluginInterface {
 
+    private final String id;
 
     public PluginAApplication() {
+        this.id = UUID.randomUUID().toString();
     }
+
+
+//    public PluginAApplication() {
+//    }
     private ConfigurableApplicationContext context;
 
     public static void main(String[] args) {
@@ -30,4 +40,16 @@ public class PluginAApplication  implements PluginInterface {
             context.close();
         }
     }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public ConfigurableApplicationContext getApplicationContext() {
+        return context;
+    }
+
+
 }
